fe35080ce82ba2c62d3e8c7c91c5669d9a7a38d8
Dhruvil Shah <dhruvilshah884@gmail.com>
2026-06-22 15:05:49 +0530

    logo functionality

--- lib/features/auth/screens/splash_screen.dart
+++ lib/features/auth/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../provider/auth_provider.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
@@ -13,7 +14,6 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Just trigger the auth check - router will handle navigation
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ref.read(authProvider.notifier).checkLogin();
     });
@@ -21,44 +21,19 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    return Scaffold(
-      backgroundColor: colorScheme.primaryContainer,
-      body: SafeArea(
-        child: Column(
-          children: [
-            // Center Content
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Image.asset(
-                      "assets/images/adaptive-icon.png",
-                      width: 180,
-                      height: 180,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
+    final width = MediaQuery.sizeOf(context).width;
+    final logoSize = (width * 0.4).clamp(145.0, 190.0);
 
-            // Bottom Text
-            Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: Text(
-                'Make in India 🇮🇳',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
-                  color: colorScheme.onPrimaryContainer,
-                ),
-              ),
-            ),
-          ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Image.asset(
+            'assets/images/splash-logo.png',
+            width: logoSize,
+            height: logoSize,
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
